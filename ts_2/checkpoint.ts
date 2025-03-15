ad6e4d71ee7e2f42d8f8930bf146fec4143452fd
roua32 <roua.zyr@gmail.com>
2025-03-15 14:43:34 +0100

    2

---
+++ ts_2/checkpoint.ts
@@ -0,0 +1,39 @@
+//CODE 1
+import React from "react";
+
+interface GreetingProps {
+  name: string;
+}
+const Greeting: React.FC<GreetingProps> = ({ name }) => {
+  return <div>Hello, {name}!</div>;
+};
+
+export default Greeting;
+
+// CODE 2
+
+import React, { Component } from "react";
+
+interface CounterState {
+  count: number;
+}
+
+class Counter extends Component<{}, CounterState> {
+  state: CounterState = {
+    count: 0,
+  };
+
+  increment = (): void => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
+}
+export default Counter;
